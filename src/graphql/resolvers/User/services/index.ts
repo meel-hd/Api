01a6084c506d9214487f502af4e687f12117c55c
Mehdi ELOUALY <92476321+meel-hd@users.noreply.github.com>
2024-02-13 12:48:54 +0100

    Refector signupUser to a seperate services

--- src/graphql/resolvers/User/services/index.ts
+++ src/graphql/resolvers/User/services/index.ts
@@ -1,6 +1,6 @@
-import { randomInt } from "crypto";
 import { Context } from "../../../../context";
 import { CreateUserInput, UpdateUserInput, User } from "../../../schema/User";
+import signupUserService from "./signupUser";
 import updateUserProfileService from "./updateProfile";
 
 
@@ -9,48 +9,11 @@ export class UserService {
     constructor(context: Context) {
         this.context = context;
     }
-    /** Signs up a user by email
-     *  if the user doesn't exist it creates it,
-     *  then, and if the user exist it creates a verifecation token
-     *  and send it to the user email.
-     *  @returns a boolean if operation was successful, false otherwise.
-     */
     async signupUser(args: CreateUserInput): Promise<boolean> {
-        let createdUser = await this.context.prisma.user.findUnique({ where: { email: args.email } })
-        // New user
-        if (createdUser == null) {
-            createdUser = await this.context.prisma.user.create({ data: { email: args.email } })
-            if (createdUser == null) {
-                return false;
-            }
-        }else {
-            // Delete old verification tokens
-            await this.context.prisma.verification_Token.deleteMany({
-                where: {
-                    targetUserId: createdUser.id
-                }
-            })
-        }
-        // Create a 6 integer long verefication token
-        const token = randomInt(100000, 999999);
-        const verification_token = await this.context.prisma.verification_Token.create({
-            data: {
-                targetUserId: createdUser.id,
-                expired: false,
-                token,
-            }
-        });
-        if(verification_token == null){
-            return false;
-        }
-
-        // TODO: Send email to the user with the verification token
-
-        // Successfull
-        return true;
+        return signupUserService(this.context, args)
     }
 
-    async updateProfile(args:UpdateUserInput): Promise<User>{
-        return updateUserProfileService(this.context,args)
+    async updateProfile(args: UpdateUserInput): Promise<User> {
+        return updateUserProfileService(this.context, args)
     }
 }
---
+++ src/graphql/resolvers/User/services/signupUser/index.ts
@@ -0,0 +1,47 @@
+import { randomInt } from "crypto";
+import { Context } from "../../../../../context";
+import { CreateUserInput } from "../../../../schema/User";
+
+
+/** Signs up a user by email
+ *  if the user doesn't exist it creates it,
+ *  then, and if the user already exists, it creates a verifecation token
+ *  and send it to the user email.
+ *  @returns a boolean if operation was successful, false otherwise.
+ */
+async function signupUserService(ctx: Context, args: CreateUserInput): Promise<boolean> {
+    let createdUser = await ctx.prisma.user.findUnique({ where: { email: args.email } })
+    // New user
+    if (createdUser == null) {
+        createdUser = await ctx.prisma.user.create({ data: { email: args.email } })
+        if (createdUser == null) {
+            return false;
+        }
+    } else {
+        // Delete old verification tokens
+        await ctx.prisma.verification_Token.deleteMany({
+            where: {
+                targetUserId: createdUser.id
+            }
+        })
+    }
+    // Create a 6 integer long verefication token
+    const token = randomInt(100000, 999999);
+    const verification_token = await ctx.prisma.verification_Token.create({
+        data: {
+            targetUserId: createdUser.id,
+            expired: false,
+            token,
+        }
+    });
+    if (verification_token == null) {
+        return false;
+    }
+
+    // TODO: Send email to the user with the verification token
+
+    // Successfull
+    return true;
+}
+
+export default signupUserService;
